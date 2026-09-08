f8f44aded66f8081f9462e50d55492033d700b9a
Reka <144533386+RekaYOO@users.noreply.github.com>
2026-09-09 00:58:46 +0800

    ci: skip redundant reusable Android artifact

--- tests/test_release_workflows.py
+++ tests/test_release_workflows.py
@@ -60,6 +60,7 @@ def test_release_dag_stays_small_and_explicit():
 def test_android_release_gate_checks_actual_runtime_and_reuses_web_build():
     assert "workflow_call:" in ANDROID
     assert "if: ${{ !inputs.frontend_artifact }}" in ANDROID
+    assert "if: ${{ github.event_name != 'workflow_call' }}" in ANDROID
     assert "name: ${{ inputs.frontend_artifact }}" in ANDROID
     assert 'python-version: "3.13"' in ANDROID
     assert ":client-app:connectedX86TestDebugAndroidTest" in ANDROID
